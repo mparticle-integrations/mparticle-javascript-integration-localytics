01ddb92f870dd390728cdaed877dbbbfbc76c496
Mo Mustafa <mmustafa@mparticle.com>
2025-03-11 16:00:41 -0700

    feat: update Localytics to v4 and remove root event forwarder

--- src/LocalyticsEventForwarder.js
+++ src/LocalyticsEventForwarder.js
@@ -78,7 +78,7 @@
                         l[i] = function() { (l[i].q = l[i].q || []).push(arguments) };
                         l[i].t = +new Date;
                         (s = y.createElement(t)).type = 'text/javascript';
-                        s.src = '//web.localytics.com/v3/localytics.js';
+                        s.src = '//web.localytics.com/v4/localytics.min.js';
                         (c = y.getElementsByTagName(t)[0]).parentNode.insertBefore(s, c);
                     }(window, document, 'script', 'll');
                     
